4e24f8b07fe291384a5f6673092b35d06d292612
Jaison Titus <jaison@hasura.io>
2017-11-16 18:01:59 +0530

    added test for custom service

--- build/test.js
+++ build/test.js
@@ -24,4 +24,24 @@ hasura.auth.signUpPromise('my-password').then(function(response) {
 }, function(error) {
   console.log('Auth Error');
   console.log(error);
-})
+});
+
+//custom service
+hasura.fetchPromise({
+  service: 'data',
+  path: '/v1/query',
+  method: 'POST',
+  json: {
+    type: 'select',
+    args: {
+      table: 'article',
+      columns: ['*']
+    }
+  }
+}).then(function(response) {
+  console.log('CS Response');
+  console.log(response);
+}, function(error) {
+  console.log('CS Error');
+  console.log(error);
+});
